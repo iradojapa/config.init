55a30186a190867ebe94a0956bfa35f557d2ae59
iradojapa <iradojapa@gmail.com>
2021-02-09 16:48:53 -0300

    instalando wsl

---
+++ wsl.ps1
@@ -0,0 +1,7 @@
+#ativando WSL
+dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+
+#habilitando VM Plataform
+dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+
+#definir wsl2 default
